810b30f3e85bb0eaeb1ef2666b8a1ddbec23170e
thijnmens <thijnsmulders04@gmail.com>
2023-12-03 19:08:55 +0100

    Day 4 2015

---
+++ 2015/Day 4/python/day4_1.py
@@ -0,0 +1,21 @@
+import hashlib
+
+def main():
+	output = -1
+
+	with open("./input.txt", 'r') as input:
+		line = input.readline()
+
+		lastHash = ""
+		while (not lastHash.startswith("00000")):
+			output += 1
+			lastHash = hashlib.md5(f"{line}{output}".encode()).hexdigest()
+
+		input.close()
+	
+	print(output)
+	
+
+
+if __name__ == "__main__":
+	main()
