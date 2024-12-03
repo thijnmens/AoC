f51dc739adf127896d850b6ae0ae760c731ca88d
thijnmens <thijnsmulders04@gmail.com>
2024-12-03 12:57:51 +0000

    2024 Day 3 solution

---
+++ 2024/Day 3/ts/day3_1.ts
@@ -0,0 +1,39 @@
+import { createReadStream } from 'node:fs';
+import { createInterface } from 'node:readline';
+
+async function main() {
+	const input = await readFile((input, line) => {
+		input.memory += line.trim()
+		return input
+	})
+
+	const matches = Array.from(input.memory.matchAll(/(mul\(\d{1,3},\d{1,3}\))/g), x=>x[1])
+
+	let output = 0
+	matches.forEach((match) => {
+		const numbers = Array.from(match.matchAll(/(\d{1,3})/g), x=>Number(x[1]))
+		output += numbers[0] * numbers[1]
+	})
+
+	console.log(output)
+}
+
+async function readFile(typeMapper: (input: InputType, line: string) => InputType): Promise<InputType> {
+	const lines = createInterface({
+		input: createReadStream('../input.txt'),
+		crlfDelay: Infinity,
+	});
+
+	let input: InputType = new InputType()
+	for await (let line of lines) {
+		input = typeMapper(input, line);
+	}
+
+	return input
+}
+
+class InputType {
+	memory: string = '';
+}
+
+main();
